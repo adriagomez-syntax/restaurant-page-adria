a04fc1034908ed07df6b46e1a020b672b04c3c6e
Adria Gomez <adria.gomez@tn.syntax-institut.de>
2026-08-11 13:21:22 +0200

    Cosmetic fix for SelectionButton

--- src/components/common/utils/SelectionButton.jsx
+++ src/components/common/utils/SelectionButton.jsx
@@ -1,13 +1,12 @@
 import Button from "./Button"
 
 export default function SelectionButton({ isActive = false, defColor, borderColor, selColor, bgColor, children }) {
-    let finalClass = "border text-xs tracking-widest font-semibold px-5 py-2 rounded-xs";
+    let finalClass = "text-xs tracking-widest font-semibold px-5 py-2 rounded-xs";
     if (isActive)   finalClass += " text-" + selColor + " bg-" + bgColor;
-    else            finalClass += " text-" + defColor + " border-" + borderColor;
+    else            finalClass += " text-" + defColor + " border border-" + borderColor;
     
     return (
-        <Button 
-            className={ finalClass }>
+        <Button className={ finalClass }>
             { children }
         </Button>
     )
